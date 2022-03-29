cdc7648802d709e9c4baeea911dfc7659af1710c
najmeh98 <foroughi.najmeh98@gmail.com>
2022-03-29 12:56:12 +0430

    add theme

---
+++ components/types/theme.ts
@@ -0,0 +1,20 @@
+export interface Theme {
+  fontSize: {
+    normal: string;
+  };
+  fontFamily: {
+    MainFont: string;
+  };
+  fontWeight: { regular: string };
+  color: {
+    background: string;
+    containerBackground: string;
+    // overlayBackground: string;
+    textcolor: string;
+    buttoncolor: string;
+    InputBgcolor: string;
+  };
+  boxShadow: string;
+  borderRadius: string;
+  height: number;
+}
---
+++ utils/theme.tsx
@@ -0,0 +1,23 @@
+import { Theme } from "../components/types/theme";
+
+export const darkTheme: Theme = {
+  fontSize: {
+    normal: "16",
+  },
+  fontWeight: {
+    regular: "400",
+  },
+  fontFamily: {
+    MainFont: "inherint",
+  },
+  color: {
+    background: "rgba(0, 0, 0, 0.2)",
+    containerBackground: "#0c4a6e",
+    textcolor: "rgba(6, 182, 212 , 1)",
+    buttoncolor: "rgb(0,0,0,1)",
+    InputBgcolor: "rgba(14 ,116 ,144, 1)",
+  },
+  boxShadow: "rgba(8,145,178,.3)",
+  borderRadius: "0.375rem",
+  height: 40,
+};
