e70a26c85e6c6aeaf4a121fac154af33983d6c0c
najmeh98 <foroughi.najmeh98@gmail.com>
2022-04-11 16:11:15 +0430

    add length of task

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { Todoprop } from "../pages";
 import { TodoTask } from "./TodoTask";
 
 interface todoListTask {
-  filterTodo: any;
+  filterTodo: [];
   onDeleteTask: (id: number) => void;
   onChangeTask: (value: string, id: number) => void;
   CompleteHandler: (id: number) => void;
@@ -15,19 +16,36 @@ export const TodoList = ({
   onChangeTask,
   CompleteHandler,
 }: todoListTask) => {
+  const [editTodo, setEditTodo] = useState<Todoprop | null>(null);
+  console.log(filterTodo);
   return (
     <>
-      {filterTodo.map((item: Todoprop, index: number) => (
-        <TodoTask
-          key={item.id}
-          item={item}
-          onDeleteTask={() => onDeleteTask(item.id)}
-          onChangeTask={() => onChangeTask(item.value, item.id)}
-          onCompleteTask={() => CompleteHandler(item.id)}
-          checked={item.completed === true}
-        />
-      ))}
+      {filterTodo &&
+        filterTodo?.length > 0 &&
+        filterTodo?.map((item: Todoprop, index: number) => {
+          console.log(item);
+          return (
+            <TodoTask
+              key={item.id}
+              item={item}
+              onDeleteTask={() => onDeleteTask(item.id)}
+              onChangeTask={onChangeTask}
+              onCompleteTask={() => CompleteHandler(item.id)}
+              checked={item.completed}
+              LengthofTask={filterTodo.length}
+            />
+          );
+        })}
     </>
+  );
+};
+
+const EmptyTask = () => {
+  return (
+    <div>
+      <p>todo list is empty dude!</p>
+      <img src="" alt="emptytask" />
+    </div>
   );
 };
 
