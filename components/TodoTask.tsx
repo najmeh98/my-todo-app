734b42f3ed66e04c60d1041cedb10cb85fe97cc1
najmeh98 <foroughi.najmeh98@gmail.com>
2022-04-12 13:47:22 +0430

    add todoTask

--- components/TodoTask.tsx
+++ components/TodoTask.tsx
@@ -1,17 +1,21 @@
 /* eslint-disable react/jsx-no-duplicate-props */
-import { Children, ReactNode, useState } from "react";
+import { Children, ReactNode, SetStateAction, useState } from "react";
 import { useTransition, animated } from "react-spring";
 import styled, { css } from "styled-components";
 import { TodoMoreIcon } from "./home/icons/icons";
 import { Modal } from "./Modal";
 import { MdDelete, MdOutlineEdit } from "react-icons/md";
 import { useTheme } from "./Context/ThemeContext";
+import { Form, TodoForm } from "./TodoForm";
+import { StandardInput } from "./Input";
+import { SaveButton } from "./SaveButton";
 type Props = {
   item: any;
   onDeleteTask: () => void;
   onChangeTask: () => void;
   onCompleteTask: () => void;
   checked: boolean;
+  LengthofTask: number;
 };
 
 export const TodoTask = ({
@@ -20,13 +24,34 @@ export const TodoTask = ({
   onChangeTask,
   onCompleteTask,
   checked,
+  LengthofTask,
 }: Props) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [newValue, setNewValue] = useState(item?.value || "");
+  let taskContent;
+
+  // if (isEditing) {
+  //   taskContent = (
+  //     <>
+  //       <TodoForm isEditing={isEditing} setIsEditing={setIsEditing} />
+  //     </>
+  //   );
+  // } else {
+  // }
+  const onClose = () => {
+    setIsOpen(!isOpen);
+  };
 
   let theme = useTheme();
   const handleMenu = (event: { stopPropagation: () => void }) => {
     event.stopPropagation();
     setIsOpen(!isOpen);
+  };
+
+  const HandleEdite = () => {
+    setIsEditing(!isEditing);
   };
 
   let transitions = useTransition(isOpen, {
@@ -37,66 +62,91 @@ export const TodoTask = ({
 
   return (
     <>
-      <TaskList
-        style={{
-          backgroundColor: theme.color.buttoncolor,
-          color: theme.color.textcolor,
-          padding: theme.padding,
-          boxShadow: theme.boxShadowItem,
-        }}
-      >
-        <Text
-          onClick={onCompleteTask}
-          style={
-            item.completed
-              ? {
-                  textDecoration: "line-through",
-                  opacity: 0.5,
-                }
-              : { opacity: 1 }
-          }
-        >
-          {item.value}
-        </Text>
-        <span onClick={handleMenu}>
-          <TodoMoreIcon />
-        </span>
-      </TaskList>
-      {transitions(
-        (style, item) =>
-          item && (
-            <Menu>
-              <animated.div
-                style={{
-                  position: "absolute",
-                  boxShadow: theme.boxShadowbox,
-                  backgroundColor: theme.color.textcolor,
-                  padding: "12px",
-                  borderRadius: theme.borderRadius,
-                  width: theme.width,
+      {isEditing ? (
+        <>
+          {/* <TodoForm
+            item={item}
+            isEditing={isEditing}
+            setIsEditing={setIsEditing}
+            submitHandler={onChangeTask}
+            setIsOpen={setIsOpen}
+            isOpen={isOpen}
+          /> */}
+          <Form onSubmit={() => onChangeTask(newValue, item?.id)}>
+            <StandardInput
+              value={newValue}
+              type="text"
+              onChange={(e: { target: { value: SetStateAction<string> } }) =>
+                setNewValue(e.target.value)
+              }
+            />
+            <SaveButton />
+          </Form>
+        </>
+      ) : (
+        <>
+          <TaskList
+            style={{
+              backgroundColor: theme.color.buttoncolor,
+              color: theme.color.textcolor,
+              padding: theme.padding,
+              boxShadow: theme.boxShadowItem,
+            }}
+          >
+            <Text
+              onClick={onCompleteTask}
+              style={
+                item.completed
+                  ? {
+                      textDecoration: "line-through",
+                      opacity: 0.5,
+                    }
+                  : { opacity: 1 }
+              }
+            >
+              {item.value}
+            </Text>
+            <span onClick={handleMenu}>
+              <TodoMoreIcon />
+            </span>
+          </TaskList>
+          {transitions(
+            (style, item) =>
+              item && (
+                <Menu>
+                  <animated.div
+                    style={{
+                      position: "absolute",
+                      boxShadow: theme.boxShadowbox,
+                      backgroundColor: theme.color.textcolor,
+                      padding: "12px",
+                      borderRadius: theme.borderRadius,
+                      width: theme.width,
 
-                  right: "22px",
-                  top: "-10px",
-                  ...style,
-                }}
-              >
-                <Item
-                  style={{ color: theme.color.colorIcon }}
-                  onClick={onDeleteTask}
-                >
-                  <p>Delete</p>
-                  <MdDelete />
-                </Item>
-                <Item
-                  style={{ color: theme.color.colorIcon }}
-                  onClick={onChangeTask}
-                >
-                  <p>Edit</p>
-                  <MdOutlineEdit />
-                </Item>
-              </animated.div>
-            </Menu>
-          )
+                      right: "22px",
+                      top: "-10px",
+                      ...style,
+                    }}
+                  >
+                    <Item
+                      style={{ color: theme.color.colorIcon }}
+                      onClick={onDeleteTask}
+                    >
+                      <p>Delete</p>
+                      <MdDelete />
+                    </Item>
+                    <Item
+                      style={{ color: theme.color.colorIcon }}
+                      onClick={HandleEdite}
+                    >
+                      <p>Edit</p>
+                      <MdOutlineEdit />
+                    </Item>
+                  </animated.div>
+                </Menu>
+              )
+          )}
+        </>
       )}
     </>
   );
