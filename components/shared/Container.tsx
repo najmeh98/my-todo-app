ef54461ca374dd730b4c000e26df573342e41d78
najmeh98 <foroughi.najmeh98@gmail.com>
2022-04-20 12:54:23 +0430

    add style

--- components/shared/Container.tsx
+++ components/shared/Container.tsx
@@ -1,10 +1,20 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { Mobile, notdesktop } from "../../utils/media";
 
 export const Row = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  width: 600px;
+  /* ${notdesktop(css`
+    width: 100%;
+    max-width: 500px;
+  `)} */
+
+  ${Mobile(css`
+    width: 100%;
+  `)}
 `;
 
 export const Column = styled.div`
